81e4c83b4063c88dba7436852e8e3f4dfb9f440e
Amr <amr.khaledt1@gmail.com>
2025-09-07 14:17:16 +0300

    feat: REPL

--- repl.go
+++ repl.go
@@ -1,8 +1,29 @@
 package main
 
 import (
+	"bufio"
+	"fmt"
+	"os"
 	"strings"
 )
+
+func startRepl() {
+	scanner := bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Printf("Pokedex > ")
+		scanner.Scan()
+
+		text := cleanInput(scanner.Text())
+		if len(text) == 0 {
+			continue
+		}
+
+		commandName := text[0]
+		fmt.Printf("Your command was: %s\n", commandName)
+
+	}
+
+}
 
 func cleanInput(text string) []string {
 	lowerWord := strings.ToLower(text)
